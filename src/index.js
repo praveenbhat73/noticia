81f44752f5c105370e2fce9003afde11012d255c
praveenbhat73 <bhatpraveen721@gmail.com>
2022-12-10 10:03:22 +0530

    initialise app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,5 +13,8 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// ReactDOM.render will take two html Element -> one is html tag and html code purpose is to display specified html code in specified html Element
+//the code -> what and all is wrapped inside app.js is rendered in html-> index.html -> div with root id
+//after the rendering this can be seen on website
+//ReactDOM.render(<p>Hello</p>, document.getElementById('root'));-> like this we use app.js which contains code 
